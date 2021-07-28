2d50135da01f09163c4965383626b507e00969d7
Krzysztof Siejkowski <siejkowski@protonmail.ch>
2021-07-28 10:38:54 +0200

    [CP-2209] Change recovery method texts in a backwards-compatible manner. Also, start treating generated Pods projects as binary files

--- libraries/CoreTranslation/Sources/Localization.swift
+++ libraries/CoreTranslation/Sources/Localization.swift
@@ -423,11 +423,29 @@ public class LocalizedString {
     /// Repeat password field title
     public lazy var _su_repeat_password_field_title = NSLocalizedString("Repeat password", bundle: Common.bundle, comment: "Repeat password field title")
 
+    // TODO: CP-2352 — remove the default value once the text is translated to all languages
     /// Signup recovery view title
-    public lazy var _su_recovery_view_title = NSLocalizedString("Set recovery method", bundle: Common.bundle, comment: "Recovery view title")
+    public lazy var _su_recovery_view_title = NSLocalizedString(
+        "Set recovery method (optional)",
+        tableName: nil,
+        bundle: Common.bundle,
+        value: _su_recovery_method_button,
+        comment: "Recovery view title"
+    )
 
+    // TODO: CP-2352 — remove the string once the new text (below) is translated to all languages
+    /// Signup recovery view description — old string
+    public lazy var _su_recovery_view_desc_old = NSLocalizedString("This will help you access your account in case you forget your password or get locked out of your account.", bundle: Common.bundle, comment: "Recovery view description (old string, replaced)")
+
+    // TODO: CP-2352 — remove the default value once the text is translated to all languages
     /// Signup recovery view description
-    public lazy var _su_recovery_view_desc = NSLocalizedString("This will help you access your account in case you forget your password or get locked out of your account.", bundle: Common.bundle, comment: "Recovery view description")
+    public lazy var _su_recovery_view_desc = NSLocalizedString(
+        "We will send recovery instructions to this email or phone number if you get locked out of your account.",
+        tableName: nil,
+        bundle: Common.bundle,
+        value: _su_recovery_view_desc_old,
+        comment: "Recovery view description"
+    )
 
     /// Signup recovery segmented email
     public lazy var _su_recovery_seg_email = NSLocalizedString("Email", bundle: Common.bundle, comment: "Recovery segmenet email")
