1a928db67f96293d290e2b17f7954729edb6cce9
Grzegorz Biegaj <grzegorz.biegaj@proton.ch>
2022-02-02 13:57:55 +0000

    Fixing generating singular and plural form from

--- libraries/CoreTranslation/Sources/Localization.swift
+++ libraries/CoreTranslation/Sources/Localization.swift
@@ -613,12 +613,6 @@ public class LocalizedString {
     
     /// Upgrade plan button
     public lazy var _pu_upgrade_plan_button = NSLocalizedString("Upgrade", bundle: Common.bundle, comment: "Upgrade plan button")
-    
-    /// Plan details price time period n months
-    public lazy var _pu_plan_details_price_time_period_m = NSLocalizedString("for %d months", bundle: Common.bundle, comment: "Plan details price time period n months")
-    
-    /// Plan details price time period n years
-    public lazy var _pu_plan_details_price_time_period_y = NSLocalizedString("for %d years", bundle: Common.bundle, comment: "Plan details price time period n years")
    
     /// Plan details renew automatically expired
     public lazy var _pu_plan_details_renew_auto_expired = NSLocalizedString("Your plan will automatically renew on %@", bundle: Common.bundle, comment: "Plan details renew automatically expired")
@@ -629,32 +623,14 @@ public class LocalizedString {
     /// Plan details unavailable contact administrator
     public lazy var _pu_plan_details_plan_details_unavailable_contact_administrator = NSLocalizedString("Contact an administrator to make changes to your Proton subscription.", bundle: Common.bundle, comment: "Plan details unavailable contact administrator")
     
-    /// Plan details n users
-    public lazy var _pu_plan_details_n_users = NSLocalizedString("%d users", bundle: Common.bundle, comment: "Plan details n users")
-    
     /// Plan details storage
     public lazy var _pu_plan_details_storage = NSLocalizedString("%@ storage", bundle: Common.bundle, comment: "Plan details storage")
 
     /// Plan details storage per user
     public lazy var _pu_plan_details_storage_per_user = NSLocalizedString("%@ storage / user", bundle: Common.bundle, comment: "Plan details storage per user")
-    
-    /// Plan details n addresses
-    public lazy var _pu_plan_details_n_addresses = NSLocalizedString("%d email addresses", bundle: Common.bundle, comment: "Plan details n addresses")
-
-    /// Plan details n addresses per user
-    public lazy var _pu_plan_details_n_addresses_per_user = NSLocalizedString("%d email addresses / user", bundle: Common.bundle, comment: "Plan details n address per user")
-
-    /// Plan details n calendars
-    public lazy var _pu_plan_details_n_calendars = NSLocalizedString("%d calendars", bundle: Common.bundle, comment: "Plan details n calendar")
-    
-    /// Plan details n folders / labels
-    public lazy var _pu_plan_details_n_folders = NSLocalizedString("%d folders / labels", bundle: Common.bundle, comment: "Plan details n folders / labels")
 
     /// Plan details medium speed
     public lazy var _pu_plan_details_vpn_free_speed = NSLocalizedString("Medium speed", bundle: Common.bundle, comment: "Plan details medium speed")
-    
-    /// Plan details n countries
-    public lazy var _pu_plan_details_countries = NSLocalizedString("%d countries", bundle: Common.bundle, comment: "Plan details n countries")
     
     /// Plan details custom email addresses
     public lazy var _pu_plan_details_custom_email = NSLocalizedString("Custom email addresses", bundle: Common.bundle, comment: "Plan details custom email addresses")
@@ -667,27 +643,6 @@ public class LocalizedString {
     
     /// Plan details adblocker
     public lazy var _pu_plan_details_streaming_service = NSLocalizedString("Streaming service support", bundle: Common.bundle, comment: "Plan details streaming service support")
-
-    /// Plan details n calendars per user
-    public lazy var _pu_plan_details_n_calendars_per_user = NSLocalizedString("%d calendars / user", bundle: Common.bundle, comment: "Plan details n calendars per user")
-
-    /// Plan details n connections
-    public lazy var _pu_plan_details_n_connections = NSLocalizedString("%d connections", bundle: Common.bundle, comment: "Plan details n connections")
-    
-    /// Plan details n VPN connections
-    public lazy var _pu_plan_details_n_vpn_connections = NSLocalizedString("%d VPN connections", bundle: Common.bundle, comment: "Plan details n VPN connections")
-
-    /// Plan details n high-speed connections
-    public lazy var _pu_plan_details_n_high_speed_connections = NSLocalizedString("%d high-speed VPN connections", bundle: Common.bundle, comment: "Plan details n high-speed connections")
-
-    /// Plan details n high-speed connections per user
-    public lazy var _pu_plan_details_n_high_speed_connections_per_user = NSLocalizedString("%d high-speed VPN connections / user", bundle: Common.bundle, comment: "Plan details n connections per user")
-
-    /// Plan details n custom domains
-    public lazy var _pu_plan_details_n_custom_domains = NSLocalizedString("%d custom domains", bundle: Common.bundle, comment: "Plan details n custom domains")
-
-    /// Plan details n addresses & calendars
-    public lazy var _pu_plan_details_n_addresses_and_calendars = NSLocalizedString("%d addresses & calendars", bundle: Common.bundle, comment: "Plan details n addresses & calendars")
 
     /// Plan details n uneven amount of addresses & calendars
     public lazy var _pu_plan_details_n_uneven_amounts_of_addresses_and_calendars = NSLocalizedString("%@ & %@", bundle: Common.bundle, comment: "Plan details n uneven amount of addresses & calendars, like: 1 address & 2 calendars")
@@ -740,5 +695,4 @@ public class LocalizedString {
     
     /// Networking connection error
     public lazy var _net_insecure_connection_error = NSLocalizedString("The TLS certificate validation failed when trying to connect to the Proton API. Your current Internet connection may be monitored. To keep your data secure, we are preventing the app from accessing the Proton API.\nTo log in or access your account, switch to a new network and try to connect again.", bundle: Common.bundle, comment: "Networking insecure connection error")
-    
 }
