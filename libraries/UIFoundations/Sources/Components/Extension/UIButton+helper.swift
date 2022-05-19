2a4c553e9ba0a738f15243efe9e4ef01c40a2ee0
AnsonChen <jianyou.chen@proton.ch>
2022-05-19 11:24:58 +0800

    Fix dynamic color return wrong color

--- libraries/UIFoundations/Sources/Components/Extension/UIButton+helper.swift
+++ libraries/UIFoundations/Sources/Components/Extension/UIButton+helper.swift
@@ -23,8 +23,14 @@ import UIKit
 
 extension UIButton {
     func setBackgroundColor(_ color: UIColor, forState controlState: UIControl.State) {
+        let resolvedColor: UIColor
+        if #available(iOS 13.0, *) {
+            resolvedColor = color.resolvedColor(with: self.traitCollection)
+        } else {
+            resolvedColor = color
+        }
         let colorImage = UIGraphicsImageRenderer(size: CGSize(width: 1, height: 1)).image { _ in
-            color.setFill()
+            resolvedColor.setFill()
             UIBezierPath(rect: CGRect(x: 0, y: 0, width: 1, height: 1)).fill()
         }
         setBackgroundImage(colorImage, for: controlState)
