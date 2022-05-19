440a821bfd372074dc4c72202337ba90cd398c3b
AnsonChen <jianyou.chen@proton.ch>
2022-05-19 11:26:15 +0800

    minor change

--- libraries/UIFoundations/Sources/Components/Extension/UIButton+helper.swift
+++ libraries/UIFoundations/Sources/Components/Extension/UIButton+helper.swift
@@ -23,11 +23,9 @@ import UIKit
 
 extension UIButton {
     func setBackgroundColor(_ color: UIColor, forState controlState: UIControl.State) {
-        let resolvedColor: UIColor
+        var resolvedColor = color
         if #available(iOS 13.0, *) {
             resolvedColor = color.resolvedColor(with: self.traitCollection)
-        } else {
-            resolvedColor = color
         }
         let colorImage = UIGraphicsImageRenderer(size: CGSize(width: 1, height: 1)).image { _ in
             resolvedColor.setFill()
