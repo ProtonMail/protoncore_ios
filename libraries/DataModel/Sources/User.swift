8e7c4f59fb5e896e14930beecd8f7baac6b8c065
Jacek Krasiukianis <jacek.krasiukianis@proton.ch>
2022-10-11 12:39:12 +0200

    MAILIOS-2855 Replace ShowImages with more granular settings

--- libraries/DataModel/Sources/User.swift
+++ libraries/DataModel/Sources/User.swift
@@ -22,7 +22,7 @@
 import Foundation
 
 public struct User: Codable, Equatable {
-    
+
     public let ID: String
     public let name: String?
     public let usedSpace: Double
@@ -93,6 +93,8 @@ public final class UserInfo: NSObject {
     public var enableFolderColor: Int
     /// 0 - threading, 1 - single message
     public var groupingMode: Int
+    public var hideEmbeddedImages: Int
+    public var hideRemoteImages: Int
     public var imageProxy: ImageProxy
     public var inheritParentFolderColor: Int
     public var language: String
@@ -105,7 +107,6 @@ public final class UserInfo: NSObject {
     public var notify: Int
     public var passwordMode: Int
     public var role: Int
-    public var showImages: ShowImages
     public var sign: Int
     /// 0: free user, > 0: paid user
     public var subscribed: Int
@@ -125,10 +126,12 @@ public final class UserInfo: NSObject {
                      keys: nil, userId: "", linkConfirmation: 0,
                      credit: 0, currency: "", subscribed: 0)
     }
-    
+
     // init from cache
     public required init(
         displayName: String?,
+        hideEmbeddedImages: Int?,
+        hideRemoteImages: Int?,
         imageProxy: Int?,
         maxSpace: Int64?,
         notificationEmail: String?,
@@ -139,7 +142,6 @@ public final class UserInfo: NSObject {
         language: String?,
         maxUpload: Int64?,
         notify: Int?,
-        showImages: Int?,
         swipeLeft: Int?,
         swipeRight: Int?,
         role: Int?,
@@ -173,7 +175,7 @@ public final class UserInfo: NSObject {
         self.delinquent = delinquent ?? DefaultValue.delinquent
         self.userKeys = keys ?? DefaultValue.userKeys
         self.userId = userId ?? DefaultValue.userId
-        
+
         // get from user settings
         self.crashReports = crashReports ?? DefaultValue.crashReports
         self.credit = credit ?? DefaultValue.credit
@@ -196,8 +198,9 @@ public final class UserInfo: NSObject {
         self.delaySendSeconds = delaySendSeconds ?? DefaultValue.delaySendSeconds
         self.displayName = displayName ?? DefaultValue.displayName
         self.groupingMode = groupingMode ?? DefaultValue.groupingMode
+        self.hideEmbeddedImages = hideEmbeddedImages ?? DefaultValue.hideEmbeddedImages
+        self.hideRemoteImages = hideRemoteImages ?? DefaultValue.hideRemoteImages
         self.imageProxy = imageProxy.map(ImageProxy.init(rawValue:)) ?? DefaultValue.imageProxy
-        self.showImages = showImages.map(ShowImages.init(rawValue:)) ?? DefaultValue.showImages
         self.sign = sign ?? DefaultValue.sign
         self.swipeLeft = swipeLeft ?? DefaultValue.swipeLeft
         self.swipeRight = swipeRight ?? DefaultValue.swipeRight
@@ -210,7 +213,7 @@ public final class UserInfo: NSObject {
         self.messageToolbarActions = messageToolbarActions ?? DefaultValue.messageToolbarActions
         self.listToolbarActions = listToolbarActions ?? DefaultValue.listToolbarActions
     }
-    
+
     // init from api
     public required init(maxSpace: Int64?,
                          usedSpace: Int64?,
@@ -236,6 +239,8 @@ public final class UserInfo: NSObject {
         self.displayName = DefaultValue.displayName
         self.enableFolderColor = DefaultValue.enableFolderColor
         self.groupingMode = DefaultValue.groupingMode
+        self.hideEmbeddedImages = DefaultValue.hideEmbeddedImages
+        self.hideRemoteImages = DefaultValue.hideRemoteImages
         self.imageProxy = DefaultValue.imageProxy
         self.inheritParentFolderColor = DefaultValue.inheritParentFolderColor
         self.language = language ?? DefaultValue.language
@@ -248,7 +253,6 @@ public final class UserInfo: NSObject {
         self.notify = DefaultValue.notify
         self.passwordMode = DefaultValue.passwordMode
         self.role = role ?? DefaultValue.role
-        self.showImages = DefaultValue.showImages
         self.sign = DefaultValue.sign
         self.subscribed = subscribed ?? DefaultValue.subscribed
         self.swipeLeft = DefaultValue.swipeLeft
@@ -261,14 +265,14 @@ public final class UserInfo: NSObject {
         self.userKeys = keys ?? DefaultValue.userKeys
         self.weekStart = DefaultValue.weekStart
     }
-    
+
     /// Update user addresses
     ///
     /// - Parameter addresses: new addresses
     public func set(addresses: [Address]) {
         self.userAddresses = addresses
     }
-    
+
     /// set User, copy the data from input user object
     ///
     /// - Parameter userinfo: New user info
@@ -289,21 +293,17 @@ public final class UserInfo: NSObject {
 // exposed interfaces
 extension UserInfo {
 
-    public var autoShowRemote: Bool {
-        showImages.contains(.remote)
-    }
-    
     public var isPaid: Bool {
         return self.role > 0 ? true : false
     }
-    
+
     public func firstUserKey() -> Key? {
         if self.userKeys.count > 0 {
             return self.userKeys[0]
         }
         return nil
     }
-    
+
     public func getPrivateKey(by keyID: String?) -> String? {
         if let keyID = keyID {
             for userkey in self.userKeys where userkey.keyID == keyID {
@@ -312,7 +312,7 @@ extension UserInfo {
         }
         return firstUserKey()?.privateKey
     }
-    
+
     @available(*, deprecated, renamed: "isKeyV2")
     internal var newSchema: Bool {
         for key in addressKeys where key.newSchema {
@@ -320,11 +320,11 @@ extension UserInfo {
         }
         return false
     }
-    
+
     public var isKeyV2: Bool {
         return addressKeys.isKeyV2
     }
-    
+
     /// TODO:: fix me - Key stuff
     public var addressKeys: [Key] {
         var out = [Key]()
@@ -340,12 +340,12 @@ extension UserInfo {
         let addr = userAddresses.address(byID: address_id) ?? userAddresses.defaultSendAddress()
         return addr?.keys.first?.privateKey ?? ""
     }
-    
+
     public func getAddressKey(address_id: String) -> Key? {
         let addr = userAddresses.address(byID: address_id) ?? userAddresses.defaultSendAddress()
         return addr?.keys.first
     }
-    
+
     /// Get all keys that belong to the given address id
     /// - Parameter address_id: Address id
     /// - Returns: Keys of the given address id. nil means can't find the address
@@ -372,6 +372,8 @@ extension UserInfo {
         static let displayName: String = ""
         static let enableFolderColor: Int = 0
         static let groupingMode: Int = 0
+        static let hideEmbeddedImages: Int = 1
+        static let hideRemoteImages: Int = 0
         static let imageProxy: ImageProxy = .imageProxy
         static let inheritParentFolderColor: Int = 0
         static let language: String = "en_US"
@@ -382,7 +384,6 @@ extension UserInfo {
         static let notify: Int = 0
         static let passwordMode: Int = 1
         static let role: Int = 0
-        static let showImages: ShowImages = .none
         static let sign: Int = 0
         static let subscribed: Int = 0
         static let swipeLeft: Int = 3
--- libraries/Login/Sources/LoginAndSignup+DataTypes.swift
+++ libraries/Login/Sources/LoginAndSignup+DataTypes.swift
@@ -36,14 +36,14 @@ public enum LoginData {
 }
 
 public extension LoginData {
-    
+
     var credential: Credential {
         switch self {
         case .userData(let userData): return Credential(userData.credential, scope: userData.scopes)
         case .credential(let credential): return credential
         }
     }
-    
+
     func updated(credential: Credential) -> LoginData {
         switch self {
         case .credential:
@@ -57,7 +57,7 @@ public extension LoginData {
                                       scopes: credential.scope))
         }
     }
-    
+
     func updated(user: User) -> LoginData {
         switch self {
         case .credential: return self
@@ -96,6 +96,8 @@ public struct UserData {
 
     public var toUserInfo: UserInfo {
         UserInfo(displayName: user.displayName,
+                 hideEmbeddedImages: nil,
+                 hideRemoteImages: nil,
                  imageProxy: nil,
                  maxSpace: Int64(user.maxSpace),
                  notificationEmail: nil,
@@ -106,7 +108,6 @@ public struct UserData {
                  language: nil,
                  maxUpload: Int64(user.maxUpload),
                  notify: nil,
-                 showImages: nil,
                  swipeLeft: nil,
                  swipeRight: nil,
                  role: user.role,
