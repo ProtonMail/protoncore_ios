ea411d31ee861f3dc6596b81feed878d598842f1
Victor Jalencas <victor.jalencas@proton.ch>
2022-10-20 17:20:43 +0200

    Updating specs for new release version 3.23.3

---
+++ Specs/ProtonCore-Authentication/3.23.3/pods_configuration.rb
@@ -0,0 +1,60 @@
+$version = "3.23.3"
+
+$git_url = ENV['PROTON_CORE_GIT_URL']
+
+$homepage = 'https://github.com/ProtonMail'
+$license = { :type => 'GPLv3', :file => 'LICENSE' }
+$author = { 'zhj4478' => 'feng@pm.me' }
+$source = { :git => $git_url, :tag => $version }
+
+$ios_deployment_target = "11.0"
+$macos_deployment_target = "10.13"
+
+$swift_versions = ['5.6']
+
+def crypto_module(symbol)
+    case symbol
+    when :crypto
+        return "ProtonCore-Crypto"
+    when :crypto_vpn
+        return "ProtonCore-Crypto-VPN"
+    else
+        raise "Unknown symbol passed to crypto_module_name function"
+    end
+end
+
+def crypto_subspec(symbol)
+    case symbol
+    when :crypto
+        return "UsingCrypto"
+    when :crypto_vpn
+        return "UsingCryptoVPN"
+    else
+        raise "Unknown symbol passed to crypto_subspec function"
+    end
+end
+
+def crypto_test_subspec(symbol)
+    case symbol
+    when :crypto
+        return "TestsUsingCrypto"
+    when :crypto_vpn
+        return "TestsUsingCryptoVPN"
+    else
+        raise "Unknown symbol passed to crypto_subspec function"
+    end
+end
+
+def no_default_subspecs(s)
+
+# Creating the default podspec with an error or warning emitting file is the workaround for https://github.com/CocoaPods/CocoaPods/issues/10264
+
+    s.default_subspecs = ['ErrorWarningEmittingDefaultSubspec']
+    s.subspec 'ErrorWarningEmittingDefaultSubspec' do |empty|
+        empty.source_files = "libraries/ErrorWarningEmittingDefaultSubspec/ErrorWarningEmittingDefaultSubspec.swift"
+    end
+end
+
+def this_pod_does_not_have_subspecs(s)
+    s.default_subspecs = []
+end
---
+++ Specs/ProtonCore-Features-V5/3.23.3/ProtonCore-Features-V5.podspec
@@ -0,0 +1,44 @@
+require_relative 'pods_configuration'
+
+Pod::Spec.new do |s|
+
+    s.name             = 'ProtonCore-Features-V5'
+    s.module_name      = 'ProtonCore_Features'
+    s.version          = $version
+    s.summary          = 'shared frameworks'
+    
+    s.description      = <<-DESC
+    ios shared frameworks for all client apps
+    DESC
+
+    s.homepage         = $homepage
+    s.license          = $license
+    s.author           = $author
+    s.source           = $source
+
+    s.ios.deployment_target = $ios_deployment_target
+    s.osx.deployment_target = $macos_deployment_target
+
+    s.swift_versions = $swift_versions
+
+    s.pod_target_xcconfig = { 'APPLICATION_EXTENSION_API_ONLY' => 'NO' }
+
+    s.dependency 'ProtonCore-DataModel'
+    s.dependency 'ProtonCore-Hash'
+
+    make_subspec = ->(spec, crypto) {
+        spec.subspec "#{crypto_subspec(crypto)}" do |subspec|
+            subspec.dependency "#{crypto_module(crypto)}", $version
+            subspec.dependency "ProtonCore-KeyManager/#{crypto_subspec(crypto)}", $version
+            subspec.dependency "ProtonCore-Authentication/#{crypto_subspec(crypto)}", $version
+            subspec.dependency "ProtonCore-Networking", $version
+            subspec.dependency "ProtonCore-Common-V5", $version
+            subspec.source_files = 'libraries/Features/Sources/*.swift'
+        end
+    }
+
+    no_default_subspecs(s)
+    make_subspec.call(s, :crypto)
+    make_subspec.call(s, :crypto_vpn)
+
+end
