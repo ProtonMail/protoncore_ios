a1b25286993f4a61d4b3642cfef4628921859144
Michal Fousek <michal.fousek@proton.ch>
2023-08-17 12:09:46 +0200

    feat(ci): Add scripts to sync repository to github

--- scripts/mirror_to_github.sh
+++ scripts/mirror_to_github.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/zsh -x
 
 if [ $# -eq 0 ]; then
   echo "No branch name provided"
@@ -11,11 +11,27 @@ fi
 RELEASE_BRANCH=$1
 
 git fetch origin --tags --force --prune
-
 git checkout --track origin/$RELEASE_BRANCH
+git pull --force origin $RELEASE_BRANCH
 
-git pull origin $RELEASE_BRANCH --rebase
+echo "$GITHUB_DEPLOY_PRIVATE_KEY" | tr -d '\r' | ssh-add - > /dev/null
 
-CURRENT_VERSION=$(cat pods_configuration.rb | grep "\$version = " | sed "s/\$version = \"//" | sed "s/\"//")
+git remote add github git@github.com:ProtonMail/protoncore_ios.git
+git push github $RELEASE_BRANCH
 
-ssh-agent bash -c "ssh-add - <<< \"$GITHUB_DEPLOY_PRIVATE_KEY\"; git remote add github git@github.com:ProtonMail/protoncore_ios.git; git remote -v; git fetch github --tags --force --prune; git push github $RELEASE_BRANCH; git push github $CURRENT_VERSION"
+# We want to sync only tags that are added after history split. So commit for every existing tag is checked. And if the commit for the tag is in $RELEASE_BRANCH then 
+# tag is pushed to the remote. 
+#
+# Now this is reasonably fast. It may happen that there will be so many tags in future that this will be slow. It will happen in a future and at that point probably 
+# all the old (pre split) tags can be deleted and this logic can be deleted also. And then all the tags will be simply pushed.
+tags=`git tag -l`
+local IFS=$'\n'
+setopt sh_word_split
+for tag in $tags; do
+    gitHash=`git rev-list -n 1 "${tag}"`
+    contains=`git branch "${RELEASE_BRANCH}" --contains "${gitHash}"`
+    if [[ ! -z "${contains}" ]]; then
+        git push github "$tag"
+    fi
+done
+
