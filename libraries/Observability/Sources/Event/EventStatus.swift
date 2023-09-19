5e9bd318ab5ff0dedbdec3ebfbb2ce732c496a67
Michal Fousek <michal.fousek@proton.ch>
2023-09-19 08:08:33 +0200

    feature(dynamicPlans): Update dynamic plans metrics

--- libraries/Observability/Sources/Event/EventStatus.swift
+++ libraries/Observability/Sources/Event/EventStatus.swift
@@ -39,6 +39,8 @@ public enum AuthenticationState: String, Encodable, CaseIterable {
 public enum HTTPResponseCodeStatus: String, Encodable, CaseIterable {
     case http2xx
     case http4xx
+    case http409
+    case http422
     case http5xx
     case unknown
 }
