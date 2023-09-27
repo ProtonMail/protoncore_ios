af45efd7dfd18442e72897d4554974b41acd4c05
Anson Chen <jianyou.chen@proton.ch>
2023-09-27 11:44:33 +0000

    MAILIOS-3589, introduce new function to verify given protection
    Co-authored-by: Michal Fousek <michal.fousek@proton.ch>
    
    Refs: https://gitlab.protontech.ch/apple/shared/protoncore/-/merge_requests/1428

--- libraries/Keymaker/Tests/KeymakerTests.swift
+++ libraries/Keymaker/Tests/KeymakerTests.swift
@@ -25,4 +25,69 @@ import XCTest
 
 class KeymakerTests: XCTestCase {
 
+    var keychainMock: KeychainMock!
+    var protectorMock: ProtectionStrategyMock!
+    var keymaker: Keymaker!
+
+    override func setUp() async throws {
+        keychainMock = KeychainMock(service: "whatever", accessGroup: "whatever")
+        keymaker = Keymaker(autolocker: nil, keychain: keychainMock)
+        protectorMock = ProtectionStrategyMock()
+        ProtectionStrategyMock.underlyingKeychainLabel = "whatever"
+        protectorMock.keychain = keychainMock
+
+        try await super.setUp()
+    }
+
+    override func tearDown() async throws {
+        keymaker = nil
+        try await super.tearDown()
+    }
+
+    func test__VerifyProtector__EverythingIsOk__KeyIsVerified() async {
+        let expectedCypherBits = Data([8, 9, 244])
+        keychainMock.dataForKeyClosure = { _ in return expectedCypherBits }
+        protectorMock.unlockCypherBitsClosure = { cypherBits in
+            XCTAssertEqual(expectedCypherBits, cypherBits)
+            return cypherBits.bytes
+        }
+
+        do {
+            try await keymaker.verify(protector: protectorMock)
+        } catch {
+            XCTFail("Verification failed with: \(error)")
+        }
+    }
+
+    func test__VerifyProtector__GettingCyberBitsFails__VerificationFails() async {
+        keychainMock.dataForKeyClosure = { _ in return nil }
+
+        do {
+            try await keymaker.verify(protector: protectorMock)
+            XCTFail("Verification should throw error.")
+        } catch {
+            guard let err = error as? Keymaker.Errors, err == .cypherBitsIsNil else {
+                XCTFail("Expected that error is Keymaker.Errors.cypherBitsIsNil but it is: \(error)")
+                return
+            }
+        }
+    }
+
+    func test__VerifyProtector__UnlockFails__VerificationFails() async {
+        let expectedCypherBits = Data([8, 9, 244])
+        keychainMock.dataForKeyClosure = { _ in return expectedCypherBits }
+        protectorMock.unlockCypherBitsClosure = { _ in
+            throw Keymaker.Errors.cypherBitsIsNil
+        }
+
+        do {
+            try await keymaker.verify(protector: protectorMock)
+            XCTFail("Verification should throw error.")
+        } catch {
+            guard let err = error as? Keymaker.Errors, err == .cypherBitsIsNil else {
+                XCTFail("Expected that error is Keymaker.Errors.cypherBitsIsNil but it is: \(error)")
+                return
+            }
+        }
+    }
 }
