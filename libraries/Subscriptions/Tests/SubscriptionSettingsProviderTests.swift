f450902632782103e9c265edcf319fab88d4f6e7
Victor Jalencas <victor.jalencas@proton.ch>
2023-10-09 14:19:07 +0200

    fix (dependencies): avoid building the iOS specific tests on a mac

--- libraries/Subscriptions/Tests/SubscriptionSettingsProviderTests.swift
+++ libraries/Subscriptions/Tests/SubscriptionSettingsProviderTests.swift
@@ -19,10 +19,13 @@
 //  You should have received a copy of the GNU General Public License
 //  along with ProtonCore.  If not, see <https://www.gnu.org/licenses/>.
 
+#if os(iOS)
 
 import XCTest
 import ProtonCorePayments
 import ProtonCoreSubscriptions
+
+
 
 final class SubscriptionSettingsProviderTests: XCTestCase {
 
@@ -62,3 +65,5 @@ final class SubscriptionSettingsProviderTests: XCTestCase {
     }
 
 }
+
+#endif
