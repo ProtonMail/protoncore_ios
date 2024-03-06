6c400fd2b1886592274276189c61ecdbf779a37f
Alex Morral <alex.morral@proton.ch>
2024-03-06 16:03:56 +0100

    chore: Update user reviewers in release script

--- scripts/create_release_mr_with_new_version.sh
+++ scripts/create_release_mr_with_new_version.sh
@@ -231,8 +231,8 @@ git add . -A
 echo "$LOG_PREFIX $ git commit -m \"[$NEW_VERSION] Releasing core version $NEW_VERSION\""
 git commit -m "[$NEW_VERSION] Releasing core version $NEW_VERSION"
 
-echo "$LOG_PREFIX $ git push origin $RELEASE_BRANCH -o merge_request.create -o merge_request.target=$TARGET_BRANCH -o merge_request.remove_source_branch -o merge_request.assign=\"vjalencas\" -o merge_request.assign=\"crolland\" -o merge_request.assign=\"eackerma\""
-git push origin $RELEASE_BRANCH -o merge_request.create -o merge_request.target=$TARGET_BRANCH -o merge_request.remove_source_branch -o merge_request.assign="vjalencas" -o merge_request.assign="crolland"  -o merge_request.assign="eackerma"
+echo "$LOG_PREFIX $ git push origin $RELEASE_BRANCH -o merge_request.create -o merge_request.target=$TARGET_BRANCH -o merge_request.remove_source_branch -o merge_request.assign=\"vjalencas\" -o merge_request.assign=\"amorral\" -o merge_request.assign=\"eackerma\""
+git push origin $RELEASE_BRANCH -o merge_request.create -o merge_request.target=$TARGET_BRANCH -o merge_request.remove_source_branch -o merge_request.assign="vjalencas" -o merge_request.assign="amorral"  -o merge_request.assign="eackerma"
 
 echo "$LOG_PREFIX $ git checkout develop"
 git checkout develop
@@ -270,8 +270,8 @@ else
   git commit -m "Updating specs for new release version $NEW_VERSION"
 fi
 
-echo "$LOG_PREFIX $ git push origin $RELEASE_BRANCH -o merge_request.create -o merge_request.target=develop -o merge_request.remove_source_branch -o merge_request.assign=\"ksiejkow\" -o merge_request.assign=\"vjalencas\" -o merge_request.assign=\"crolland\" -o merge_request.assign=\"eackerma\""
-git push origin $RELEASE_BRANCH -o merge_request.create -o merge_request.target=develop -o merge_request.remove_source_branch -o merge_request.assign="ksiejkow" -o merge_request.assign="vjalencas" -o merge_request.assign="crolland" -o merge_request.assign="eackerma"
+echo "$LOG_PREFIX $ git push origin $RELEASE_BRANCH -o merge_request.create -o merge_request.target=develop -o merge_request.remove_source_branch -o merge_request.assign=\"vjalencas\" -o merge_request.assign=\"amorral\" -o merge_request.assign=\"eackerma\""
+git push origin $RELEASE_BRANCH -o merge_request.create -o merge_request.target=develop -o merge_request.remove_source_branch -o merge_request.assign="vjalencas" -o merge_request.assign="amorral" -o merge_request.assign="eackerma"
 
 
 
