4323b630e5b2a8f8d31c97bec44706dbd89d31a1
Erik Ackermann <erik.ackermann@proton.ch>
2024-11-07 12:27:22 +0000

    Revert "fix(debug): print fetched feature flag values when upserting"
    This reverts merge request !1957

--- libraries/FeatureFlags/Sources/FeatureFlagsRepository.swift
+++ libraries/FeatureFlags/Sources/FeatureFlagsRepository.swift
@@ -159,11 +159,6 @@ public extension FeatureFlagsRepository {
         }
 
         let (flags, userID) = try await remoteDataSource.getFlags()
-
-        let flagsDescription = flags.map { "\($0.name)=\($0.enabled)" }.joined(separator: ", ")
-
-        PMLog.debug("Initializing flags: \(flagsDescription)")
-
         localDataSource.value.upsertFlags(.init(flags: flags), userId: userID)
     }
 
