3ad2c95581eae2c6f934a3ccbb1fe642d6c4f5a5
Alex Morral <alex.morral@proton.ch>
2024-11-27 16:09:18 +0100

    feat(SSO): CP-8995 Create use case for VerifyUnprivatization

---
+++ libraries/Login/Sources/Services/Models/UnprivatizationInfo.swift
@@ -0,0 +1,77 @@
+//
+//  ActivateAuthDeviceRequest.swift
+//  ProtonCore-Login - Created on 13.11.24.
+//
+//  Copyright (c) 2024 Proton Technologies AG
+//
+//  This file is part of Proton Technologies AG and ProtonCore.
+//
+//  ProtonCore is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ProtonCore is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with ProtonCore.  If not, see <https://www.gnu.org/licenses/>.
+
+import ProtonCoreCrypto
+import ProtonCoreNetworking
+
+public final class UnprivatizationInfo: Response, APIDecodableResponse, Encodable {
+    var state: UnprivatizeState
+    var adminEmail: String
+    var orgKeyFingerprintSignature: ArmoredSignature
+    var orgPublicKey: ArmoredKey
+
+    public init(
+        state: UnprivatizeState,
+        adminEmail: String,
+        orgKeyFingerprintSignature: ArmoredSignature,
+        orgPublicKey: ArmoredKey
+    ) {
+        self.state = state
+        self.adminEmail = adminEmail
+        self.orgKeyFingerprintSignature = orgKeyFingerprintSignature
+        self.orgPublicKey = orgPublicKey
+    }
+
+    public init(from decoder: any Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        state = try container.decode(UnprivatizeState.self, forKey: .state)
+        adminEmail = try container.decode(String.self, forKey: .adminEmail)
+        let unArmoredOrgSignature = try container.decode(String.self, forKey: .orgKeyFingerprintSignature)
+        orgKeyFingerprintSignature = ArmoredSignature(value: unArmoredOrgSignature)
+        let unarmoredOrgPublicKey = try container.decode(String.self, forKey: .orgPublicKey)
+        orgPublicKey = ArmoredKey(value: unarmoredOrgPublicKey)
+    }
+
+    public func encode(to encoder: any Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(state, forKey: .state)
+        try container.encode(adminEmail, forKey: .adminEmail)
+        try container.encode(orgKeyFingerprintSignature.value, forKey: .orgKeyFingerprintSignature)
+        try container.encode(orgPublicKey.value, forKey: .orgPublicKey)
+    }
+
+    required init() {
+        fatalError("init() has not been implemented")
+    }
+    
+    public enum UnprivatizeState: Int, Codable {
+        case declined
+        case pending
+        case ready
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case state
+        case adminEmail
+        case orgKeyFingerprintSignature
+        case orgPublicKey
+    }
+}
---
+++ libraries/Login/Sources/Services/UseCases/VerifyUnprivatization.swift
@@ -0,0 +1,99 @@
+//
+//  VerifyUnprivatization.swift
+//  ProtonCore-Login - Created on 26.11.24.
+//
+//  Copyright (c) 2024 Proton Technologies AG
+//
+//  This file is part of Proton Technologies AG and ProtonCore.
+//
+//  ProtonCore is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  ProtonCore is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with ProtonCore.  If not, see <https://www.gnu.org/licenses/>.
+
+import Foundation
+import ProtonCoreAuthentication
+import ProtonCoreCrypto
+import ProtonCoreDataModel
+import ProtonCoreServices
+
+/// Called as a first step in the unprivatization flow for first time login SSO users.
+///
+/// The user needs to be unprivatized so that org Admin has access.
+struct VerifyUnprivatization {
+
+    typealias AuthenticationManager = AuthenticatorInterface
+
+    let apiService: APIService
+    let authManager: AuthenticationManager
+
+    private enum Constants {
+        static let orgVerificationContext = "account.organization-fingerprint"
+    }
+
+    init(apiService: APIService) {
+        self.apiService = apiService
+        self.authManager = Authenticator(api: apiService)
+    }
+
+    func invoke() async throws -> UnprivatizeUserSuccess {
+        // Fetch the route GET /core/v4/members/me/unprivatize to get the data info.
+        let unprivatizationInfoRequest = UnprivatizationInfoRequest()
+        let (_, unprivatizationInfo): (_, UnprivatizationInfo) = try await apiService.perform(request: unprivatizationInfoRequest)
+
+        guard unprivatizationInfo.state == .pending else { throw UnprivatizationError.invalidState }
+
+        // Fetch the keys from GET /keys/all for the AdminEmail.
+        let adminEmail = unprivatizationInfo.adminEmail
+        guard let keysInfo = try await authManager.getKeys(email: adminEmail, internalOnly: false),
+              let publicAddressKey = keysInfo.address.keys.first else {
+            // If unable, or the key is not in the Address part (i.e. in KT) error out.
+            throw UnprivatizationError.publicAddressKeyNotFound
+        }
+
+        // Verify the SHA265 fingerprint of the OrgPublicKey using the OrgKeyFingerprintSignature and the fetched admin address key.
+        let fingerprint = unprivatizationInfo.orgPublicKey.sha256Fingerprint
+
+        let verified = try Sign.verifyDetached(
+            signature: unprivatizationInfo.orgKeyFingerprintSignature,
+            plainText: fingerprint,
+            verifierKey: ArmoredKey(value: publicAddressKey.publicKey),
+            trimTrailingSpaces: false,
+            verificationContext: VerificationContext(
+                value: Constants.orgVerificationContext,
+                required: .always
+            )
+        )
+
+        guard verified else { throw UnprivatizationError.verificationError }
+
+        return UnprivatizeUserSuccess(
+            adminEmail: adminEmail,
+            organizationPublicKey: unprivatizationInfo.orgPublicKey
+        )
+    }
+}
+
+public struct UnprivatizeUserSuccess {
+    let adminEmail: String
+    let organizationPublicKey: ArmoredKey
+
+    public init(adminEmail: String, organizationPublicKey: ArmoredKey) {
+        self.adminEmail = adminEmail
+        self.organizationPublicKey = organizationPublicKey
+    }
+}
+
+enum UnprivatizationError: Error {
+    case invalidState
+    case publicAddressKeyNotFound
+    case verificationError
+}
