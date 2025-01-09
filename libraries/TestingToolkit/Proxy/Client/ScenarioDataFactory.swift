4436ef4d60b022f3504196fcb6139838a430f209
Richard Gasztany <richard.gasztany@proton.ch>
2025-01-09 22:02:38 +0100

    TPE-907: Update client support scenario folder and add disable forward option

--- libraries/TestingToolkit/Proxy/Client/ScenarioDataFactory.swift
+++ libraries/TestingToolkit/Proxy/Client/ScenarioDataFactory.swift
@@ -24,7 +24,6 @@ import Foundation
 public class ScenarioDataFactory {
 
     public static func readScenarioFile(filename: String, subdirectory: String, bundle: Bundle) throws -> ScenarioFileWithName {
-
         guard let data = bundle.getDataFor(fileName: filename, subdirectory: subdirectory) else {
             throw ScenarioDataError.missingFile("Missing scenario file: \(filename) in folder: \(subdirectory)")
         }
@@ -37,7 +36,6 @@ public class ScenarioDataFactory {
             }
 
             return ScenarioFileWithName(name: filename, directory: subdirectory, scenarioFile: scenarioFile)
-
         } catch {
             throw ScenarioDataError.jsonParsingError("Failed to read or parse scenario file: \(error.localizedDescription)")
         }
@@ -47,34 +45,76 @@ public class ScenarioDataFactory {
         var parsedMockRoutes: [MockObject] = []
 
         for mockFile in scenarioFileWithName.scenarioFile.mockFiles {
-            guard let data = bundle.getDataFor(fileName: mockFile, subdirectory: scenarioFileWithName.directory) else {
-                throw ScenarioDataError.missingFile("Missing mock file: \(mockFile) in folder: \(scenarioFileWithName.directory)")
-            }
+            if let fileURL = bundle.url(forResource: mockFile, withExtension: nil) {
+                var isDirectory: ObjCBool = false
 
-            do {
-                // Ensure that we add the 'name' field and enabled to the mock data before decoding
-                var jsonObject = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] ?? [:]
-                jsonObject["name"] = "\(scenarioFileWithName.name)-\(mockFile)"
-                jsonObject["enabled"] = true
-                let updatedData = try JSONSerialization.data(withJSONObject: jsonObject, options: [])
-                let generatedMock = try JSONDecoder().decode(MockObject.self, from: updatedData)
-
-                try generatedMock.request.validate()
-
-                parsedMockRoutes.append(
-                    MockObject(
-                        name: generatedMock.name,
-                        enabled: generatedMock.enabled,
-                        updateFile: generatedMock.updateFile,
-                        request: generatedMock.request,
-                        response: generatedMock.response
-                    )
-                )
-            } catch {
-                throw ScenarioDataError.jsonParsingError("Failed to read or parse mock file '\(mockFile)': \(error.localizedDescription)")
+                if FileManager.default.fileExists(atPath: fileURL.path, isDirectory: &isDirectory), isDirectory.boolValue {
+                    // Treat as a folder
+                    let folderRoutes = try processDirectory(directory: mockFile, scenarioFileWithName: scenarioFileWithName, bundle: bundle)
+                    parsedMockRoutes.append(contentsOf: folderRoutes)
+                } else {
+                    // Treat as a single file
+                    guard let mock = try processFile(fileURL: fileURL, directory: scenarioFileWithName.directory, scenarioName: scenarioFileWithName.name, bundle: bundle) else {
+                        throw ScenarioDataError.missingFile("Missing mock file: \(mockFile) in folder: \(scenarioFileWithName.directory)")
+                    }
+                    parsedMockRoutes.append(mock)
+                }
+            } else {
+                throw ScenarioDataError.missingFile("Missing resource: \(mockFile) in bundle")
             }
         }
 
         return parsedMockRoutes
+    }
+
+    private static func processFile(fileURL: URL, directory: String, scenarioName: String, bundle: Bundle) throws -> MockObject? {
+        guard fileURL.pathExtension == "json",
+              let data = bundle.getDataFor(fileName: fileURL.lastPathComponent, subdirectory: directory) else {
+            return nil
+        }
+
+        var jsonObject = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] ?? [:]
+
+        // "name" cannot be null
+        if jsonObject["name"] == nil {
+            let relativePath = fileURL.path.replacingOccurrences(of: bundle.bundlePath, with: "").trimmingCharacters(in: .whitespacesAndNewlines)
+            jsonObject["name"] = "\(scenarioName)-\(relativePath)"
+        }
+        jsonObject["enabled"] = true
+
+        let updatedData = try JSONSerialization.data(withJSONObject: jsonObject, options: [])
+        let generatedMock = try JSONDecoder().decode(MockObject.self, from: updatedData)
+        try generatedMock.request.validate()
+
+        return MockObject(
+            name: generatedMock.name,
+            enabled: generatedMock.enabled,
+            updateFile: generatedMock.updateFile,
+            request: generatedMock.request,
+            response: generatedMock.response
+        )
+    }
+
+
+    private static func processDirectory(directory: String, scenarioFileWithName: ScenarioFileWithName, bundle: Bundle) throws -> [MockObject] {
+        guard let directoryURL = bundle.url(forResource: directory, withExtension: nil) else {
+            throw ScenarioDataError.missingFile("Missing directory: \(directory)")
+        }
+
+        let fileManager = FileManager.default
+        let contents = try fileManager.contentsOfDirectory(at: directoryURL, includingPropertiesForKeys: nil, options: [.skipsHiddenFiles])
+        var mockRoutes: [MockObject] = []
+
+        for fileURL in contents {
+            if fileURL.hasDirectoryPath {
+                let subdirectoryPath = "\(directory)/\(fileURL.lastPathComponent)"
+                let subdirectoryRoutes = try processDirectory(directory: subdirectoryPath, scenarioFileWithName: scenarioFileWithName, bundle: bundle)
+                mockRoutes.append(contentsOf: subdirectoryRoutes)
+            } else if let mock = try processFile(fileURL: fileURL, directory: directory, scenarioName: scenarioFileWithName.name, bundle: bundle) {
+                mockRoutes.append(mock)
+            }
+        }
+
+        return mockRoutes
     }
 }
