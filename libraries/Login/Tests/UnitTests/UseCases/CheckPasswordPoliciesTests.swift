4d98aeb5ba43bc6c349c2e71b620cdb8318c6284
Erik Ackermann <erik.ackermann@proton.ch>
2025-05-06 16:54:56 +0200

    feat(passwordPolicy): Add Password Policy view and view model.  Add new assets.

--- libraries/Login/Tests/UnitTests/UseCases/CheckPasswordPoliciesTests.swift
+++ libraries/Login/Tests/UnitTests/UseCases/CheckPasswordPoliciesTests.swift
@@ -155,35 +155,19 @@ final class CheckPasswordPoliciesTests: XCTestCase {
     }
 
     private func atLeastOneNumberPolicy() -> PasswordPolicy {
-        return PasswordPolicy.init(policyName: "AtLeastOneNumber",
-                                   state: .enabled,
-                                   requirementMessage: "Numbers",
-                                   errorMessage: "Password must contain at least 1 number",
-                                   regex: "/[0-9]/")
+        return PasswordPolicy.atLeastOneNumberMock
     }
 
     private func atLeastOneSpecialCharacterPolicy() -> PasswordPolicy {
-        return PasswordPolicy.init(policyName: "AtLeastOneSpecialCharacter",
-                                   state: .enabled,
-                                   requirementMessage: "Symbols (!&*)",
-                                   errorMessage: "Password must contain at least 1 special character (!&*)",
-                                   regex: "/[\\*\\.\\!\\@\\$\\%\\^\\&\\#'\\\"\\`\\(\\)\\{\\}\\[\\]\\:\\;\\<\\>\\,\\.\\?\\/\\~\\_\\+\\-\\=\\|\\\\]/")
+        return PasswordPolicy.atLeastOneSpecialCharacterMock
     }
 
     private func atLeastOneUpperCaseAndOneLowercasePolicy() -> PasswordPolicy {
-        return PasswordPolicy.init(policyName: "AtLeastOneUpperCaseAndOneLowercase",
-                                   state: .enabled,
-                                   requirementMessage: "At least 1 uppercase and 1 lowercase letter",
-                                   errorMessage: "Password must contain at least 1 uppercase and 1 lowercase letter",
-                                   regex: "/(?=.*[a-z])(?=.*[A-Z])/")
+        return PasswordPolicy.atLeastOneUpperCaseAndOneLowercaseMock
     }
 
     private func disallowCommonPasswordsPolicy() -> PasswordPolicy {
-        return PasswordPolicy.init(policyName: "DisallowCommonPasswords",
-                                   state: .enabled,
-                                   requirementMessage: "Something not too common",
-                                   errorMessage: "Password shouldn't be too common or too predictable",
-                                   regex: "/^(?:(?!proton|protonmail|protonvpn|protondrive|protonpass|123456|password|12345678|qwerty|123456789|12345|1234|111111|1234567|dragon|123123|baseball|abc123|football|monkey|letmein|696969|shadow|master|666666|qwertyuiop|123321|mustang|1234567890|michael|654321|pussy|superman|1qaz2wsx|7777777|fuckyou|121212|000000|qazwsx|123qwe|killer|trustno1|jordan|jennifer|zxcvbnm|asdfgh|hunter|buster|soccer|harley|batman|andrew|tigger|sunshine|iloveyou|fuckme|2000|charlie|robert|thomas|hockey|ranger|daniel|starwars|klaster|112233|george|asshole|computer|michelle|jessica|pepper|1111|zxcvbn|555555|11111111|131313|freedom|777777|pass|fuck|maggie|159753|aaaaaa|ginger|princess|joshua|cheese|amanda|summer|love|ashley|6969|nicole|chelsea|biteme|matthew|access|yankees|987654321|dallas|austin|thunder|taylor|matrix).)*$/")
+        return PasswordPolicy.disallowCommonPasswordsMock
     }
 }
 #endif
