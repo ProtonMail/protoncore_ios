bee9acfb0632878725319ec306a6cd332f10dffc
Anson Chen <jianyou.chen@proton.ch>
2025-11-04 03:55:18 +0000

    [DRVIOS-3963] Introduce some quark commands
    Refs: https://gitlab.protontech.ch/apple/shared/protoncore/-/merge_requests/2284

--- libraries/QuarkCommands/Sources/Commands/UserCommands.swift
+++ libraries/QuarkCommands/Sources/Commands/UserCommands.swift
@@ -26,6 +26,12 @@ private let usersCreateAddress = "quark/raw::user:create:address"
 private let usersExpireSessions = "quark/raw::user:expire:sessions"
 private let usersDelete = "quark/raw::user:delete"
 private let usersCreateSubUser = "quark/raw::user:create:subuser"
+private let userResetPassword = "quark/user:reset"
+private let createAddressKey = "quark/user:create:address-key"
+private let createAccountKey = "quark/user:create:account-key"
+private let createOrganization = "quark/user:create:organization"
+private let createDomain: String = "quark/organization:create:domain"
+private let createSubUser: String = "quark/user:create:subuser"
 
 public extension Quark {
 
@@ -119,6 +125,155 @@ public extension Quark {
             .build()
 
         return try executeQuarkRequest(request)
+    }
+
+    @discardableResult
+    func resetPassword(id: Int) throws -> (data: Data, response: URLResponse) {
+        let args = [
+            "userID=\(id)",
+            "newPassword=b"
+        ]
+
+        let request = try route(userResetPassword)
+            .args(args)
+            .build()
+
+        return try executeQuarkRequest(request)
+    }
+
+    @discardableResult
+    func createNewAddressKey(id: Int, password: String, addressId: Int) throws -> (data: Data, response: URLResponse) {
+        let args = [
+            "userId=\(id)",
+            "password=\(password)",
+            "addressId=\(addressId)",
+            "--primary=true",
+        ]
+
+        let request = try route(createAddressKey)
+            .args(args)
+            .build()
+
+        return try executeQuarkRequest(request)
+    }
+
+    @discardableResult
+    func createNewAccountKey(id: Int, password: String) throws -> (data: Data, response: URLResponse) {
+        let args = [
+            "--userId=\(id)",
+            "--password=\(password)",
+            "--primary=true",
+        ]
+
+        let request = try route(createAccountKey)
+            .args(args)
+            .build()
+
+        return try executeQuarkRequest(request)
+    }
+
+    @discardableResult
+    func createNewOrganization(id: Int, password: String) throws -> Int {
+        let args = [
+            "userID=\(id)",
+            "password=\(password)"
+        ]
+
+        let request = try route(createOrganization)
+            .args(args)
+            .build()
+
+        let (textData, urlResponse) = try executeQuarkRequest(request)
+
+        guard let httpResponse = urlResponse as? HTTPURLResponse,
+              httpResponse.statusCode == 200 else {
+            throw QuarkResponse.invalidResponse
+        }
+
+        guard let htmlResponse = String(data: textData, encoding: .utf8) else {
+            throw QuarkResponse.decodingFailed
+        }
+
+        let regexPattern = #"OrganizationID:\s*(\d+)"#
+        let regex = try NSRegularExpression(pattern: regexPattern)
+        let matches = regex.matches(in: htmlResponse, range: NSRange(htmlResponse.startIndex..., in: htmlResponse))
+
+        if matches.isEmpty {
+            throw QuarkError(urlResponse: urlResponse, message: "Failed to fetch ID: \(htmlResponse)")
+        }
+
+        guard let match = matches.first else {
+            throw QuarkError(urlResponse: urlResponse, message: "No valid match found: \(htmlResponse)")
+        }
+
+        guard let idRange = Range(match.range(at: 1), in: htmlResponse) else {
+            throw QuarkError(urlResponse: urlResponse, message: "Failed to extract ID range: \(htmlResponse)")
+        }
+
+        let id = Int(htmlResponse[idRange]) ?? 0
+        return id
+    }
+
+    @discardableResult
+    func createNewDomain(id: Int) throws -> (data: Data, response: URLResponse) {
+        let args = [
+            "organization-id=\(id)"
+        ]
+
+        let request = try route(createDomain)
+            .args(args)
+            .build()
+
+        return try executeQuarkRequest(request)
+    }
+
+    @discardableResult
+    func createNewSubUser(user: User, ownerUser: User, alsoPublic: Bool = true) throws -> String {
+
+        let args = [
+            "ownerUserID=\(ownerUser.id!)",
+            "ownerPassword=\(ownerUser.password)",
+            "-N=\(user.name)",
+            "-p=\(user.password)",
+            "--private=\(alsoPublic ? 0 : 1)",
+            "-k=Curve25519"
+        ]
+
+        let request = try route(createSubUser)
+            .args(args)
+            .build()
+
+        let (textData, urlResponse) = try executeQuarkRequest(request)
+
+        guard let httpResponse = urlResponse as? HTTPURLResponse,
+              httpResponse.statusCode == 200 else {
+            throw QuarkResponse.invalidResponse
+        }
+
+        guard let htmlResponse = String(data: textData, encoding: .utf8) else {
+            throw QuarkResponse.decodingFailed
+        }
+
+        let pattern = #"Email:\s*([A-Z0-9._%+-]+@dummydomain\d+\.com)"#
+        let regex = try? NSRegularExpression(pattern: pattern, options: [.caseInsensitive])
+            let range = NSRange(htmlResponse.startIndex..<htmlResponse.endIndex, in: htmlResponse)
+
+            if let match = regex?.firstMatch(in: htmlResponse, options: [], range: range),
+               let emailRange = Range(match.range(at: 1), in: htmlResponse) {
+                return String(htmlResponse[emailRange])
+        }
+        throw EmailError.notFound
+    }
+}
+
+public extension Quark {
+    enum QuarkResponse: Error {
+        case invalidResponse
+        case decodingFailed
+    }
+
+    enum EmailError: Error {
+        case notFound
     }
 }
 
