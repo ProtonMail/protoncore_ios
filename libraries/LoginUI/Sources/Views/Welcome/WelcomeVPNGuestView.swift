fc5cb42dff247c66bc008306d5801f3d6c6a745c
Pawel Jurczyk <pawel.jurczyk@proton.ch>
2026-09-02 15:56:32 +0200

    fix: Fixes iPad login screen
    Jira-Id: VPNAPPL-3642

--- libraries/LoginUI/Sources/Views/Welcome/WelcomeVPNGuestView.swift
+++ libraries/LoginUI/Sources/Views/Welcome/WelcomeVPNGuestView.swift
@@ -21,7 +21,6 @@ import SwiftUI
 import ProtonCoreUIFoundations
 
 public struct WelcomeVPNGuestView: View {
-    @Environment(\.horizontalSizeClass) private var orientation
     @StateObject var viewModel: ViewModel
     var externalLinks = ExternalLinks(clientApp: .vpn)
 
@@ -32,80 +31,40 @@ public struct WelcomeVPNGuestView: View {
         static let noLogsImageSize: CGFloat = 20
         static let extraLargeSpacing: CGFloat = 24
         static let elementsWidthLimit: CGFloat = 480
+        static let artworkHeightLimit: CGFloat = 600
+        static let artworkOffset: CGFloat = 72
     }
 
     public var body: some View {
-        ZStack {
-            VStack {
+            VStack(spacing: 0) {
                 ZStack {
-                    IconProvider.vpnWelcomeImageV2Bg
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .if(orientation == .regular, transform: { view in
-                            view.aspectRatio(contentMode: .fill)
-                        })
-                        .frame(maxWidth: .infinity)
-                        .ignoresSafeArea(.all)
-                    IconProvider.vpnWelcomeImageV2
-                        .resizable()
-                        .aspectRatio(contentMode: .fit)
-                        .frame(maxWidth: Constants.elementsWidthLimit)
-                }
-                Spacer()
-            }
-            VStack(spacing: Constants.extraLargeSpacing) {
-                Spacer() // Two Spacers on purpose to center the content on the 2/3 of the screen
-                Spacer()
-                Link(destination: externalLinks.certifiedNoLogsVPN) {
-                    HStack {
-                        Image("certified-no-logs", bundle: .module)
+                    Color.clear
+                        .overlay {
+                            IconProvider.vpnWelcomeImageV2Bg
+                                .resizable()
+                        }
+                        .clipped()
+                        .ignoresSafeArea()
+
+                    VStack {
+                        Spacer(minLength: 0)
+                        IconProvider.vpnWelcomeImageV2
                             .resizable()
-                            .frame(width: Constants.noLogsImageSize, height: Constants.noLogsImageSize)
-                        Text(LUITranslation.login_vpn_guest_certified_no_logs.l10n)
-                            .font(.subheadline)
-                            .fontWeight(.semibold)
-                        IconProvider.arrowOutSquare
-                            .resizable()
-                            .frame(width: Constants.largeSpacing, height: Constants.largeSpacing)
+                            .aspectRatio(contentMode: .fit)
+                            .frame(maxWidth: Constants.elementsWidthLimit)
+                            .offset(y: Constants.artworkOffset)
                     }
-                    .padding(Constants.standardSpacing)
-                    .background(ColorProvider.BackgroundSecondary)
-                    .cornerRadius(Constants.mediumSpacing)
                 }
-                .buttonStyle(.plain)
+                .frame(maxHeight: Constants.artworkHeightLimit) // prevents iPad from oversizing this view
 
-                VStack(spacing: Constants.standardSpacing) {
-                    Text(LUITranslation.login_vpn_guest_screen_title.l10n)
-                        .font(.title)
-                        .fontWeight(.bold)
-                        .foregroundStyle(ColorProvider.TextNorm)
-                    Text(LUITranslation.login_vpn_guest_screen_description.l10n)
-                        .multilineTextAlignment(.center)
-                        .foregroundStyle(ColorProvider.TextWeak)
-                }
-
-                actionButtons
+                titleAndButtons
                     .frame(maxWidth: Constants.elementsWidthLimit)
-
-                Text(
-                    try! AttributedString(
-                        markdown: LUITranslation.lion_login_vpn_guest_tc_pp_description(
-                            tcLink: externalLinks.termsAndConditions.absoluteString,
-                            ppLink: externalLinks.privacyPolicy.absoluteString
-                        )
-                    )
-                )
-                    .foregroundStyle(ColorProvider.TextWeak)
-                    .tint(ColorProvider.TextAccent) // link color
-                    .multilineTextAlignment(.center)
-                    .font(.caption)
-                    .frame(maxWidth: .infinity, alignment: .center)
-
-                Spacer()
+                Spacer(minLength: 0)
             }
-            .padding()
-        }
-        .background(ColorProvider.BackgroundNorm)
+            .background {
+                ColorProvider.BackgroundNorm
+                    .ignoresSafeArea()
+            }
         .bannerDisplayable(
             bannerState: $viewModel.bannerState,
             configuration: .init(position: .bottom)
@@ -132,6 +91,58 @@ public struct WelcomeVPNGuestView: View {
         .onAppear {
             viewModel.measureOnViewDisplayed()
         }
+    }
+
+    var titleAndButtons: some View {
+        VStack(spacing: Constants.extraLargeSpacing) {
+            Link(destination: externalLinks.certifiedNoLogsVPN) {
+                HStack {
+                    Image("certified-no-logs", bundle: .module)
+                        .resizable()
+                        .frame(width: Constants.noLogsImageSize, height: Constants.noLogsImageSize)
+                    Text(LUITranslation.login_vpn_guest_certified_no_logs.l10n)
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                    IconProvider.arrowOutSquare
+                        .resizable()
+                        .frame(width: Constants.largeSpacing, height: Constants.largeSpacing)
+                }
+                .padding(Constants.standardSpacing)
+                .background(ColorProvider.BackgroundSecondary)
+                .cornerRadius(Constants.mediumSpacing)
+            }
+            .buttonStyle(.plain)
+
+            VStack(spacing: Constants.standardSpacing) {
+                Text(LUITranslation.login_vpn_guest_screen_title.l10n)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundStyle(ColorProvider.TextNorm)
+                Text(LUITranslation.login_vpn_guest_screen_description.l10n)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(ColorProvider.TextWeak)
+            }
+
+            actionButtons
+
+            Text(
+                try! AttributedString(
+                    markdown: LUITranslation.lion_login_vpn_guest_tc_pp_description(
+                        tcLink: externalLinks.termsAndConditions.absoluteString,
+                        ppLink: externalLinks.privacyPolicy.absoluteString
+                    )
+                )
+            )
+            .fixedSize(horizontal: false, vertical: true)
+            .foregroundStyle(ColorProvider.TextWeak)
+            .tint(ColorProvider.TextAccent) // link color
+            .multilineTextAlignment(.center)
+            .font(.caption)
+            .frame(maxWidth: .infinity, alignment: .center)
+            .padding(.bottom)
+        }
+        .padding()
     }
 
     @ViewBuilder
